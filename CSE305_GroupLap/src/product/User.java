159cdae479973690ac4dde55cabdb9ca884589de
ngminhnguyen <minhnguyen2811200909@gmail.com>
2023-11-18 14:30:16 +0700

    Nguyen

--- CSE305_GroupLap/src/product/User.java
+++ CSE305_GroupLap/src/product/User.java
@@ -1,5 +1,21 @@
+
 package product;
 
+
 public class User {
+    String userID;
+    String password;
+    String userName;
+    String mail;
+    int phoneNumber;
+
+    public User(String userID, String password, String userName, String mail, int phoneNumber) {
+        this.userID = userID;
+        this.password = password;
+        this.userName = userName;
+        this.mail = mail;
+        this.phoneNumber = phoneNumber;
+    }
+    
     
 }
